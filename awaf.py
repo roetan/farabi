273cce690f308ff18b7c4efb4b913100354061a6
roetan <rudytanjung@gmail.com>
2026-05-28 08:51:26 +0700

    Update awaf.py

--- awaf.py
+++ awaf.py
@@ -4,7 +4,7 @@ from functools import partial
 from pathlib import Path
 from urllib.parse import quote, quote_plus, urljoin
 
-Cache, Time, get_logger, leagues, network
+from main import Cache, Time, get_logger, leagues, network
 
 log = get_logger(__name__)
 
